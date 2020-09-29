8d1224e1500318a7d32f21a2009208fcb7b7cafa
TripleS <triple-s-76@outlook.com>
2020-09-29 07:16:31 -0700

    finished palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function() {
-
+const palindromes = function(x) {
+    let forwardsLowerCase = x.toLowerCase();
+    let forwards = '';
+    let backwards = '';
+    let character = '';
+    let length = x.length;
+    
+    
+    for (y = 0, z = 1;y < length; y++, z++) {
+        character = forwardsLowerCase.slice(y,z);
+        if (/[a-z]/.test(character)) {
+            forwards += character;
+        }
+    };
+    for (y = forwards.length - 1, z = forwards.length; y >= 0; y--, z--) {
+        character = forwards.slice(y, z);
+        backwards += character;
+    }
+    if (forwards == backwards) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = palindromes
