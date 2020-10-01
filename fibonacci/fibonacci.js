977fd8706dbf43fb9d0fec42af259aa7a3d15cc9
TripleS <triple-s-76@outlook.com>
2020-10-01 07:46:57 -0700

    finished fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+    let num1 = 1;
+    let num2 = 1;
+    let num3 = 2;
+    let numX = '';
+
+    if (x < 1) {
+        return 'OOPS';
+    } else if ((x == 1) || (x == 2)) {
+        return 1;
+    } else {
+        for (i = 3; i < x; i++) {
+            numX = num2 + num3;
+            num1 = num2;
+            num2 = num3;
+            num3 = numX;
+        };
+    return num3;
+    };
 }
 
 module.exports = fibonacci
